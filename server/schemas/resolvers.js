26c32ef96f042e7dad4a504b8d1dc995d64a5821
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-11-23 17:29:26 -0800

    create schemas content

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,84 @@
+const { Book, User } = require("../models");
+const { signToken, AuthenticationError } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      console.log("context", context);
+      if (context.user) {
+        return User.findById(args.user._id);
+      }
+      throw new AuthenticationError("You need to be logged in");
+    },
+  },
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      console.log("args", args);
+
+      const user = await User.create({
+        username,
+        email,
+        password,
+      });
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      console.log("email", email);
+      console.log("password", password);
+
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw AuthenticationError;
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw AuthenticationError;
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    saveBook: async (parent, args, context) => {
+      console.log("args", args);
+      console.log("context", context);
+
+      const user = await User.findById(context.user._id).populate("savedBooks");
+
+      if (!user) {
+        throw new AuthenticationError("You need to be logged in");
+      }
+
+      user.savedBooks.push(args.book);
+
+      return user;
+    },
+    removeBook: async (parent, args, context) => {
+      console.log("args", args);
+      console.log("context", context);
+
+      const user = await User.findById(context.user._id).populate("savedBooks");
+
+      if (!user) {
+        throw new AuthenticationError("You need to be logged in");
+      }
+
+      const book = await Book.findOne({ bookId: args.bookId });
+
+      const response = await user.savedBooks.id(book._id).deleteOne();
+
+      if (!response.ok) {
+        throw new Exception("delete failed");
+      }
+
+      return user;
+    },
+  },
+};
+
+module.exports = { resolvers };
